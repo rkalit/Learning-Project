b4172927b2559931ab012c33232d29d61f9ecd5e
rkalit <47394978+rkalit@users.noreply.github.com>
2026-08-11 08:14:45 +0700

    fix credential problem

--- service-discovery/terraform/provider.tf
+++ service-discovery/terraform/provider.tf
@@ -5,12 +5,13 @@ terraform {
       version = "~> 6.0"
     }
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "~> 2.0"
     }
   }
 }
 
 provider "aws" {
-  region = "ap-southeast-3"
+  region  = "ap-southeast-3"
+  profile = "terraform-lab"
 }
